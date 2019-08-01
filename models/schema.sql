20a22952041e4864ffbe1fe1c666da9445e69e2f
unknown <incontextseo@gmail.com>
2019-08-01 00:37:58 -0500

    Admin Page and index.js file started

--- models/schema.sql
+++ models/schema.sql
@@ -2,6 +2,5 @@ DROP DATABASE IF EXISTS cookies_db;
 CREATE DATABASE cookies_db;
 
 
-
 -- DROP DATABASE IF EXISTS classes_db;
 -- CREATE DATABASE classes_db;
--- models/seed.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO gallery (name, keywords, description, image, category) 
-VALUES ("Sawyer's Baby Shower - It's a Girl Hawaiian Cookies", "hawaiian, palm frawns, onesie, hibiscus", "default description", "baby_shower_girl_hawaiian_onsie_cookies.jpg", "Birthday");
-INSERT INTO gallery (name, keywords, description, image, category) 
-VALUES ("Paxton's 2nd Birthday - Two the Moon Cookies", "2, two, star, moon, boy, blue, big brother", "default description", "custom_birthday_boy_moon_cookies.jpg", "Birthday");
