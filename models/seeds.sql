002c3e9a46f6f422680a737c048719edd1f10d38
bryan <bthusius@gmail.com>
2019-08-05 18:09:14 -0500

    add

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,6 +1,45 @@
 USE cookies_db;
 
+
 INSERT INTO cookies (name, keywords, description, image, category) 
 VALUES ("Sawyer's Baby Shower - It's a Girl Hawaiian Cookies", "hawaiian, palm frawns, onesie, hibiscus", "default description", "baby_shower_girl_hawaiian_onsie_cookies.jpg", "Birthday");
 INSERT INTO cookies (name, keywords, description, image, category) 
 VALUES ("Paxton's 2nd Birthday - Two the Moon Cookies", "2, two, star, moon, boy, blue, big brother", "default description", "custom_birthday_boy_moon_cookies.jpg", "Birthday");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Jammin'for Jess - Airplane Iowa Cookies", "default description", "custom_airplane_cookies.jpg", "Birthday");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Onsie Baby Cookies", "2, two, boy, bib, otter", "default description", "custom_baby_shower_bib_onsie_otter_cookies.jpg", "Birthday");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Baby Shower Flower Cookies", "flowers, hearts, onesie,", "default description", "custom_baby_shower_bottle_bib_onesie_hearts_flowers_cookies.jpg", "Birthday");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Dream Weaver Baby Shower Cookies", "2, arrows, dreamweaver, baby, shower", "default description", "custom_birthday_boy_moon_cookies.jpg", "Birthday");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Sunflower Bridal Shower Cookies", "default description", "custom_bridal_shower_sunflower_cookies.jpg", "Bridal Shower");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Cactus Cookies", "default description", "custom_cactus_onsie_cookies.jpg", "Baby Shower");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Minnie Mouse Cookies", "default description", "custom_disney_minnie_mouse_birthday_cookies.jpg", "Birthday");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Nursing Graduation Cookies", "default description", "custom_nurse_graduation_flower_cookies.jpg", "Graduation");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Unicorn Rainbow Cookies", "default description", "custom_rainbow_unicron_fourth_birthday_cookies.jpg", "Birthday");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Unicorn Rose Cookies", "default description", "custom_roses_unicorn_cookies.jpg", "Birthday");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Custom Bridesmaid Cookies", "default description", "custom_will_you_be_my_bridesmaid_cookies.jpg", "Bridal Shower");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Fortnite Birthday Cookies", "default description", "fortnite_birthday_floss_dance_cookies.jpg", "Birthday");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Love Heart Flower Cookies", "default description", "love_heart_flower_gift_box_cookies.jpg", "Birthday");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Nightmare Before Christmas Cookies", "default description", "nightmare_before_christmas_cookies.jpg", "Halloween");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Heartbeat Fundraiser Cookies", "default description","nurse_doctor_heartbeat_fundraiser_thank_you_cookies.jpg", "Thank You");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Police Graduation Cookies", "default description","police_graduation_handcuffs_congrats_cookies.jpg", "Graduation");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Flower Cookies", "default description", "roses_carnations_cookies.jpg", "Birthday");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Unicorn and Dino Cookies", "default description", "unicorn_dinosaure_cookies.jpg", "Birthday");
+INSERT INTO cookies (name, keywords, description, image, category) 
+VALUES ("Scrubs and Xray Cookies", "default description", "xray_skeleton_doctor_scrubs_cookies.jpg", "Graduation");
